85eecb5f2bb91242eaaed8ea5419f462bd04e7cf
ParkJinSeok <pjsjja34@gmail.com>
2024-03-20 15:33:20 +0900

    Revert "first rest api class"
    This reverts commit ecc7ff6bac916ea3ae5502b05e05bbd5022eb7e4.

--- src/main/java/com/boot3/myrestapi/lectures/LectureRepository.java
+++
@@ -1,9 +0,0 @@
-package com.boot3.myrestapi.lectures;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface LectureRepository extends JpaRepository<Lecture, Integer> {
-    List<Lecture> findByNameContaining(String name);
-}
--- src/main/java/com/boot3/myrestapi/lectures/LectureStatus.java
+++
@@ -1,5 +0,0 @@
-package com.boot3.myrestapi.lectures;
-
-public enum LectureStatus {
-    DRAFT, PUBLISHED, BEGAN_ENROLLMENT
-}
