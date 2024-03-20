5a52f368c9c6d02e6fe4fc220223b985e8f2fcb4
ParkJinSeok <pjsjja34@gmail.com>
2024-03-20 15:46:14 +0900

    first rest api class controllre 추가

--- src/main/java/com/boot3/myrestapi/lectures/LectureController.java
+++ src/main/java/com/boot3/myrestapi/lectures/LectureController.java
@@ -1,6 +1,8 @@
 package com.boot3.myrestapi.lectures;
 
+import com.boot3.myrestapi.lectures.dto.LectureReqDto;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
@@ -17,19 +19,25 @@ import java.net.URI;
 @RequiredArgsConstructor
 public class LectureController {
     private final LectureRepository lectureRepository;
+    private final ModelMapper modelMapper;
+
 //    @Autowired 쓰는대신 아래처럼 직접 초기화 하면 좋은점은 Test할때 유용
 //    public LectureController(LectureRepository lectureRepository) {
 //        this.lectureRepository = lectureRepository;
 //    }
 
     @PostMapping
-    public ResponseEntity<?> createLecture(@RequestBody Lecture lecture) {
+    public ResponseEntity<?> createLecture(@RequestBody LectureReqDto lectureReqDto) {
+        //ReqDto => Entity 매핑
+        Lecture lecture = modelMapper.map(lectureReqDto, Lecture.class);
         Lecture addLecture = this.lectureRepository.save(lecture);
+
 
         WebMvcLinkBuilder selfLinkBuilder = WebMvcLinkBuilder.linkTo(LectureController.class).slash(addLecture.getId());
         URI createUri = selfLinkBuilder.toUri();
         return ResponseEntity.created(createUri).body(addLecture);
 
+//        DB환경 없을경우 테스트 목업 데이터
 //        lecture.setId(10);
 //        WebMvcLinkBuilder selfLinkBuilder = WebMvcLinkBuilder.linkTo(LectureController.class).slash(lecture.getId());
 //        URI createUri = selfLinkBuilder.toUri();
