d8b83be2416dab2b8201661a3eee00866e045e49
ParkJinSeok <pjsjja34@gmail.com>
2024-03-21 16:47:57 +0900

    first rest api class security 토큰 필터 추가 및 경로 변경

---
+++ src/main/java/com/boot3/myrestapi/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,34 @@
+package com.boot3.myrestapi.security.filter;
+
+import com.boot3.myrestapi.security.jwt.JwtService;
+import com.boot3.myrestapi.security.userinfo.UserInfoUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtService jwtService;
+    @Autowired
+    private UserInfoUserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        String token = null;
+        String username = null;
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            token = authHeader.substring(7);
+            username = jwtService.extractUsername(token);
+        }
+    }
+}
