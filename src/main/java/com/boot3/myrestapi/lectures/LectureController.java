195c4e267ce9fcbf7031a512260eb4a65cccdc98
ParkJinSeok <pjsjja34@gmail.com>
2024-03-21 09:33:06 +0900

    first rest api class ErrorsResource 추가후 Controller 에 상속 후 실행 하면 Error 를 뿌려줄때 링크를 뿌려주게 된다

--- src/main/java/com/boot3/myrestapi/lectures/LectureController.java
+++ src/main/java/com/boot3/myrestapi/lectures/LectureController.java
@@ -1,5 +1,6 @@
 package com.boot3.myrestapi.lectures;
 
+import com.boot3.myrestapi.common.errors.ErrorsResource;
 import com.boot3.myrestapi.lectures.dto.LectureReqDto;
 import com.boot3.myrestapi.lectures.dto.LectureResDto;
 import com.boot3.myrestapi.lectures.dto.LectureResource;
@@ -77,8 +78,8 @@ public class LectureController {
 //        return ResponseEntity.created(createUri).body(lecture);
     }
 
-    private static ResponseEntity<Errors> badRequest(Errors errors) {
-        return ResponseEntity.badRequest().body(errors);
+    private static ResponseEntity<ErrorsResource> badRequest(Errors errors) {
+        return ResponseEntity.badRequest().body(new ErrorsResource(errors));
     }
 
 }
