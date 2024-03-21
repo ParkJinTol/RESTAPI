adaf7c5c8121a89665f720e76711ac5c6192be08
ParkJinSeok <pjsjja34@gmail.com>
2024-03-21 15:04:27 +0900

    first rest api class security 새로운 user 추가 기능 controller 에 추가

--- src/main/java/com/boot3/myrestapi/security/userinfo/UserInfoController.java
+++ src/main/java/com/boot3/myrestapi/security/userinfo/UserInfoController.java
@@ -1,14 +1,27 @@
 package com.boot3.myrestapi.security.userinfo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserInfoController {
-@GetMapping("/welcome")
-public String welcome() {
-return "Welcome this endpoint is not secure";
-}
+    @Autowired
+    private UserInfoRepository repository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome this endpoint is not secure";
+    }
+
+    @PostMapping("/new")
+    public String addNewUser(@RequestBody UserInfo userInfo) {
+        userInfo.setPassword(
+                passwordEncoder.encode(userInfo.getPassword()));
+        UserInfo savedUserInfo = repository.save(userInfo);
+        return savedUserInfo.getName() + " user added!!";
+    }
 }
